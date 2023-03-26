56334b15e907543aaa27be14cb621ebe55dd06b2
Gabriel Figueiredo <gabrielcarolino962@gmail.com>
2023-03-25 22:49:12 -0300

    gpt-3.5-turbo

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-import requests
+import os
+import openai
 
-api_key =  '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'
-endpoint = 'https://api.openai.com/v1/completions'
+openai.api_key = 'my-api-key'
 
-search = input('Pergunta: ')
-model = 'text-davinci-003'
+prompt = input('Pergunta: ')
 
-data = {
-    'prompt': search,
-    'model': model,
-    'temperature': 0,
-    'max_tokens': 2048    
-}
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {
+        "role": "user", 
+        "content": prompt
+    }
+  ]
+)
 
-response = requests.post(endpoint, json=data, headers={
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {api_key}'
-})
-
-if response.ok:
-    resposta = response.json()['choices'][0]['text']
-    print(resposta)
-else:
-    print(f"Erro na solicitação: {response.text}")
+print(completion.choices[0].message.content)
