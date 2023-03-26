eb63c55ca1aa7ce10e299b162a888af741ea4537
Gabriel Figueiredo <gabrielcarolino962@gmail.com>
2023-03-25 22:43:20 -0300

    venv

---
+++ main/main.py
@@ -0,0 +1,25 @@
+import requests
+
+api_key =  'my-api-key'
+endpoint = 'https://api.openai.com/v1/completions'
+
+search = input('Pergunta: ')
+model = 'text-davinci-003'
+
+data = {
+    'prompt': search,
+    'model': model,
+    'temperature': 0,
+    'max_tokens': 2048    
+}
+
+response = requests.post(endpoint, json=data, headers={
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {api_key}'
+})
+
+if response.ok:
+    resposta = response.json()['choices'][0]['text']
+    print(resposta)
+else:
+    print(f"Erro na solicitação: {response.text}")
